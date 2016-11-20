65649a6ce543e82bb9cb94743c01efb33a7a9491
tamarous <532487608@qq.com>
2016-11-21 00:59:35 +0800

    解决了两个问题：1，CollectionView上边和状态栏重叠。2，CollectionView下边被Tab Bar遮挡。

--- RuiSi/RuiSi/Section/Controller/SectionViewController.m
+++ RuiSi/RuiSi/Section/Controller/SectionViewController.m
@@ -13,42 +13,57 @@ NSString *kSectionCollectionViewCell = @"SectionCollectionViewCell";
 NSString *kSectionHeaderViewCell = @"SectionHeaderViewCell";
 
 @interface SectionViewController()
-@property (nonatomic,strong) NSArray *forumArray;
+@property (nonatomic,strong) NSArray *itemArray;
 @property (nonatomic,strong) NSArray *sectionArray;
+- (void) setupArray;
 @end
 
 @implementation SectionViewController
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];    
-    _forumArray = @[@[@"文章天地",@"心灵花园",@"校园交易",@"我是女生",@"版聊专区",@"缘聚睿思",@"失物招领",@"西电问答",@"校园活动",@"我要毕业了",@"就业招聘"],@[@"技术博客",@"学习交流",@"软件技术交流区",@"嵌入式技术交流区",@"考研交流",@"竞赛交流",@"Abroad & English"],@[@"灌水专区",@"摄影天地",@"原创精品区",@"西电后街",@"我爱运动",@"音乐纵贯线",@"绝对漫域",@"游间客栈",@"大话体坛",@"邀请专区"],@[@"腾讯创新俱乐部",@"华为创新俱乐部"],@[@"电影",@"剧集",@"音乐",@"动漫",@"游戏",@"综艺",@"体育",@"软件",@"学习",@"纪录片",@"其他",@"西电",@"求种专区",@"新手试种专区",@"资源回收站"]];
-    
-    _sectionArray = @[@"西电生活",@"学术交流",@"休闲娱乐",@"社团风采专区",@"BT资源"];
+    [super viewDidLoad];
+    [self setupArray];
+
    
     [self.collectionView registerNib:[UINib nibWithNibName:kSectionCollectionViewCell bundle:nil] forCellWithReuseIdentifier:kSectionCollectionViewCell];
     [self.collectionView registerNib:[UINib nibWithNibName:kSectionHeaderViewCell bundle:nil] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:kSectionHeaderViewCell];
+    
+    // 这三行代码可以解决ScrollView被Tab Bar遮挡的问题
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.extendedLayoutIncludesOpaqueBars = NO;
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    
+    
     self.collectionView.backgroundColor = [UIColor colorWithRed:0.91 green:0.93 blue:0.93 alpha:1];
+    
     UICollectionViewLayout *layout = self.collectionView.collectionViewLayout;
     UICollectionViewFlowLayout *flow = (UICollectionViewFlowLayout *)layout;
     flow.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
     flow.headerReferenceSize = CGSizeMake(100, 35);
     flow.minimumLineSpacing = 1.0;
     flow.minimumInteritemSpacing = 1.0;
-    UIEdgeInsets contentInsets = self.collectionView.contentInset;
-    contentInsets.top = 20;
-    [self.collectionView setContentInset:contentInsets];
+    [self.collectionView setContentInset:UIEdgeInsetsMake(0, 0, 0, 0)];
     
 }
 
+
+- (void)setupArray {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"section" ofType:@"plist"];
+    _itemArray = [NSArray arrayWithContentsOfFile:path];
+    _sectionArray = @[@"西电生活",@"学术交流",@"休闲娱乐",@"社团风采专区",@"BT资源"];
+}
+
+
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
-    return _forumArray.count;
+    return _itemArray.count;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    NSArray *sectionArray = [_forumArray objectAtIndex:section];
+    NSArray *sectionArray = [_itemArray objectAtIndex:section];
     return sectionArray.count;
 }
 
@@ -62,11 +77,12 @@ NSString *kSectionHeaderViewCell = @"SectionHeaderViewCell";
 
 
 - (void) configureCell:(SectionCollectionViewCell *)collectionCell forItemAtIndexPath:(NSIndexPath *)indexPath {
-    collectionCell.titleLable.text = _forumArray[indexPath.section][indexPath.row];
+    collectionCell.titleLable.text = _itemArray[indexPath.section][indexPath.row];
     collectionCell.titleLable.font = [UIFont systemFontOfSize:12];
+    collectionCell.countLable.font = [UIFont systemFontOfSize:15];
     collectionCell.backgroundColor = [UIColor colorWithRed:1.0 green:1.0  blue:1.0 alpha:1];
     collectionCell.countLable.text = @"0";
-    collectionCell.countLable.font = [UIFont systemFontOfSize:15];
+    
     [collectionCell configureImageViewForIndexPath:indexPath];
 }
 
--- RuiSi/RuiSi/Section/View/SectionCollectionViewCell.m
+++ RuiSi/RuiSi/Section/View/SectionCollectionViewCell.m
@@ -14,9 +14,7 @@
 {
     if(self = [super initWithFrame:frame])
     {
-      //  _button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
-      //  [_button setImage:[UIImage imageNamed:@"common_561_icon"] forState:UIControlStateNormal];
-      //  [_button setTitle:@"XX" forState:UIControlStateNormal];
+        
     }
     return self;
 }
@@ -25,9 +23,6 @@
     
     NSString *fileName = [NSString stringWithFormat:@"%ld_%ld",(indexPath.section+1),(indexPath.row+1)];
     
-   // NSLog(@"%ld_%ld.png",(indexPath.section+1),(indexPath.row+1));
-    
     self.iconImageView.image = [UIImage imageNamed:fileName];
-    //NSLog(@"%@",self.iconImageView.image);
 }
 @end
