21a1d5fe46380caf922dd2c875b03675ff4d7cda
tamarous <532487608@qq.com>
2016-11-24 00:49:46 +0800

    添加了“关于我”的界面。

---
+++ RuiSi/RuiSi/Section/Controller/AboutMeViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutMeViewController.h
+//  RuiSi
+//
+//  Created by 汪泽伟 on 2016/11/24.
+//  Copyright © 2016年 aloes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutMeViewController : UITableViewController
+
+@end
