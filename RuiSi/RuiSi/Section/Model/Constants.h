28afeda6acdc03212e06f4baa82523250627edc0
tamarous <532487608@qq.com>
2017-04-16 21:34:19 +0800

    准备开始使用自动布局。

--- RuiSi/RuiSi/Section/Model/Constants.h
+++ RuiSi/RuiSi/Section/Model/Constants.h
@@ -9,8 +9,8 @@
 #ifndef Constants_h
 #define Constants_h
 
-#define kPublicNetURL @"http://bbs.rs.xidian.me/"
-#define kSchoolNetURL @"http://rs.xidian.edu.cn/"
+//#define kPublicNetURL @"http://bbs.rs.xidian.me/"
+//#define kSchoolNetURL @"http://rs.xidian.edu.cn/"
 #define kDevice_Is_iPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) : NO)
 #define kDevice_Is_iPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
 #define kDevice_Is_iPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) : NO)
@@ -20,5 +20,9 @@
 #define kScreen_Width [UIScreen mainScreen].bounds.size.width
 #define userDefaults [NSUserDefaults standardUserDefaults]
 
+static NSString * const kLoginSuccessNotification = @"LoginSuccessNotification";
+static NSString * const kLogoutSuccessNotification = @"LogoutSuccessNotification";
+static NSString * const kPublicNetURL = @"http://bbs.rs.xidian.me";
+static NSString * const kSchoolNetURL = @"http://rs.xidian.edu.cn/";
 
 #endif /* Constants_h */
